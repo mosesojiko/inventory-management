cdbc34a7c4915aa5e046ecab0425d4a34bdfa6fe
fejiro <fejis.daniel@gmail.com>
2021-07-28 04:36:33 -0700

    updated user route

--- routes/User.js
+++ routes/User.js
@@ -1,13 +1,25 @@
 const express = require('express');
-const { createUser, loginUser, getUsers } = require('../controllers/User')
+const { createUser, loginUser, getUsers, updateUserInfo, getSingleUser, deleteUser } = require('../controllers/User')
 const verifyToken = require('../middlewares/verifyToken')
 const router = express();
 
+//route create a user
 router.post('/signup', createUser);
+
+//route to login a user
 router.post('/login', verifyToken, loginUser);
+
+//route to get all user
 router.get('/', getUsers)
 
+//route to get user by id
+router.get('/:id', getSingleUser)
 
+//route to update a user using user id
+router.put('/update/:id', verifyToken, updateUserInfo)
+
+//router to delete user using user id
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 
 module.exports = router;
